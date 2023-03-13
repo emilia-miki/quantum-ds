18bf213a1655be67093bb213fb61faeea0bba802
mykytko <nd235471@gmail.com>
2023-03-13 17:01:13 +0200

    Add comments

--- 1/main.py
+++ 1/main.py
@@ -1,6 +1,8 @@
 import sys
 
 try:
+    # get input fromthe arguments if provided, 
+    # otherwise get it from the termial
     match len(sys.argv):
         case 1:
             N = int(input("Enter N: "))
@@ -13,11 +15,18 @@ except ValueError:
     print('Input is not an integer')
     sys.exit(0)
 
+# check if it's positive
 if N <= 0:
     print('Input is not a positive integer')
     sys.exit(0)
 
-result = sum(range(1, N + 1))
+# calculate the sum
+result = 0
+for i in range(1, N + 1):
+    result += i
+
+# here's a functional alternative:
+# result = sum(range(1, N + 1))
 
 print(f'Result: {result}')
         
